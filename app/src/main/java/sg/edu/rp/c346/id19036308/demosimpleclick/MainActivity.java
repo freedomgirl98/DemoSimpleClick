94d95807c8c16cc99e275f1de0124766c8c545c2
LEK QIU RONG <19036308@myrp.edu.sg>
2020-05-08 11:59:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19036308/demosimpleclick/MainActivity.java
@@ -0,0 +1,58 @@
+package sg.edu.rp.c346.id19036308.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Step 1: Declare the field variables
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+    ToggleButton tbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Step 2: Link the field variables to UI components in layout
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+        tbtn = findViewById(R.id.toggleButton);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                etInput.getText().toString();
+                String stringResponse = etInput.getText().toString();
+
+
+
+            }
+
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code for the action
+                if(tbtn.isChecked()){
+                    etInput.setEnabled(true);
+                } else {
+                    etInput.setEnabled(false);
+                }
+            }
+        });
+
+    }
+}
